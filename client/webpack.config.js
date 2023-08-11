eaba3509b42bae3c22b95bd978a22ac87b0aab1e
Gcreach <gcreach97@gmail.com>
2023-08-10 18:32:20 -0700

    fixed srcsw and icon

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,18 +23,18 @@ module.exports = () => {
         title: 'Text Editor'
       }),
 
-      new InjectManifest(),
+      new InjectManifest({ swSrc: './src-sw.js', swDest: 'src-sw.js' }),
       new WebpackPwaManifest({
         name: 'text editor',
         short_name: 'text editor',
         description: 'Allows you to write out text!',
         background_color: '#7eb4e2',
         theme_color: '#7eb4e2',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve('assets/images/logo.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
